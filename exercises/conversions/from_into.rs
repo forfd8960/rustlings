89c48481baa3ae980bbc4d0f3c9c900c66e62883
forfd8960 <forfd8960@gmail.com>
2023-04-04 09:32:14 +0800

    Fix From_Into

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -35,7 +35,6 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of Person
 // Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
 
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
@@ -45,15 +44,15 @@ impl From<&str> for Person {
 
         let person_split = s.split(",");
         let data: Vec<&str> = person_split.collect();
-        if data.len() == 0 {
-            return Person::default();
-        }
-
-        if data.len() != 2 {
+        if data.len() == 0 || data.len() != 2 {
             return Person::default();
         }
 
         let name = data[0];
+        if name.len() == 0 {
+            return Person::default();
+        }
+
         let age_result = data[1].parse::<usize>();
         if let Ok(age_int) = age_result {
             return Person{
